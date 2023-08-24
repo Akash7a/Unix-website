31e54c59be58532d14c4baab92717c13d5643f41
Akash Kumar <akashkumar914a@gmail.com>
2023-08-25 04:21:22 +0530

    first commit

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector(".navbar");
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+    navLinks.style.left = "-100%";
+}
